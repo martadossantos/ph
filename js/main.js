7520b5159bc3b48b353e92d2d3f44fa2ab4116eb
piggypatrol <68651278+piggypatrol@users.noreply.github.com>
2022-04-28 17:16:30 +0100

    wrap function in dom content loaded

--- js/main.js
+++ js/main.js
@@ -3,51 +3,55 @@ let imageNo = 0;
 
 mouseEntered = false
 
-image.addEventListener('mouseenter', function() {
-   mouseEntered = true
-})
-
-image.addEventListener('ontouchstart', function() {
-   mouseEntered = true
-})
-
-image.addEventListener('mouseleave', function() {
-   mouseEntered = false;
-   window.requestAnimationFrame(step);
-})
-
-image.addEventListener('ontouchend', function() {
-   mouseEntered = false;
-   window.requestAnimationFrame(step);
-})
-
-
-function updateImage(imageNo) {
-   let image_url = '/assets/images/0'+imageNo+'.jpg';
-
-   image.setAttribute('src', image_url)
-
-}
-
-function step() {
-
-   if (mouseEntered === false) {
-
-      updateImage(imageNo);
-      imageNo++;
-    
-      if (imageNo <= 84) {
-        window.requestAnimationFrame(step);
-      } else {
-         imageNo = 1;
-         window.requestAnimationFrame(step);
-      }
-
+window.addEventListener('DOMContentLoaded', (event) => {
+   image.addEventListener('mouseenter', function() {
+      mouseEntered = true
+   })
+   
+   image.addEventListener('ontouchstart', function() {
+      mouseEntered = true
+   })
+   
+   image.addEventListener('mouseleave', function() {
+      mouseEntered = false;
+      window.requestAnimationFrame(step);
+   })
+   
+   image.addEventListener('ontouchend', function() {
+      mouseEntered = false;
+      window.requestAnimationFrame(step);
+   })
+   
+   
+   function updateImage(imageNo) {
+      let image_url = '/assets/images/0'+imageNo+'.jpg';
+   
+      image.setAttribute('src', image_url)
+   
    }
+   
+   function step() {
+   
+      if (mouseEntered === false) {
+   
+         updateImage(imageNo);
+         imageNo++;
+       
+         if (imageNo <= 84) {
+           window.requestAnimationFrame(step);
+         } else {
+            imageNo = 1;
+            window.requestAnimationFrame(step);
+         }
+   
+      }
+   
+   }
+   
+   window.requestAnimationFrame(step);
+   
+});
 
-}
-
-window.requestAnimationFrame(step);
 
 
 
