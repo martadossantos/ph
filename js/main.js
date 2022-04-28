b67d85ae59d5d7124273da87424321e19c799dca
piggypatrol <68651278+piggypatrol@users.noreply.github.com>
2022-04-28 15:52:42 +0100

    new js

--- js/main.js
+++ js/main.js
@@ -4,16 +4,19 @@ let imageNo = 0;
 function updateImage(imageNo) {
    let image_url = '/assets/images/0'+imageNo+'.jpg';
 
-   if (image.length > 0) {
-      image.setAttribute('src', image_url)
-   }
+   image.setAttribute('src', image_url)
+
 }
 
 function step() {
   updateImage(imageNo);
   imageNo++;
-  if (imageNo < 85) {
+
+  if (imageNo <= 84) {
     window.requestAnimationFrame(step);
+  } else {
+     imageNo = 1;
+     window.requestAnimationFrame(step);
   }
 }
 
