d87ea67d3d1602749847131a1bb42c2cab4c2448
piggypatrol <68651278+piggypatrol@users.noreply.github.com>
2022-04-28 17:00:16 +0100

    smaller file sizes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,27 @@
 let image = document.querySelector('#img');
 let imageNo = 0;
 
+mouseEntered = false
+
+image.addEventListener('mouseenter', function() {
+   mouseEntered = true
+})
+
+image.addEventListener('ontouchstart', function() {
+   mouseEntered = true
+})
+
+image.addEventListener('mouseleave', function() {
+   mouseEntered = false;
+   window.requestAnimationFrame(step);
+})
+
+image.addEventListener('ontouchend', function() {
+   mouseEntered = false;
+   window.requestAnimationFrame(step);
+})
+
+
 function updateImage(imageNo) {
    let image_url = '/assets/images/0'+imageNo+'.jpg';
 
@@ -9,15 +30,25 @@ function updateImage(imageNo) {
 }
 
 function step() {
-  updateImage(imageNo);
-  imageNo++;
 
-  if (imageNo <= 84) {
-    window.requestAnimationFrame(step);
-  } else {
-     imageNo = 1;
-     window.requestAnimationFrame(step);
-  }
+   if (mouseEntered === false) {
+
+      updateImage(imageNo);
+      imageNo++;
+    
+      if (imageNo <= 84) {
+        window.requestAnimationFrame(step);
+      } else {
+         imageNo = 1;
+         window.requestAnimationFrame(step);
+      }
+
+   }
+
 }
 
 window.requestAnimationFrame(step);
+
+
+
+
