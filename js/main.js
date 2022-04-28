438877b44ecd2e0c22015ecea49dfc3f6cd85541
piggypatrol <68651278+piggypatrol@users.noreply.github.com>
2022-04-28 18:18:24 +0100

    add delay

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 let image = document.querySelector('#img');
 let imageNo = 0;
 
+let delay = 250;
+
 mouseEntered = false
 
 window.addEventListener('DOMContentLoaded', (event) => {
+
    image.addEventListener('mouseenter', function() {
       mouseEntered = true
    })
@@ -14,20 +17,28 @@ window.addEventListener('DOMContentLoaded', (event) => {
    
    image.addEventListener('mouseleave', function() {
       mouseEntered = false;
-      window.requestAnimationFrame(step);
+      
+      setTimeout(function() {
+         window.requestAnimationFrame(step);
+      }, delay)
+
    })
    
    image.addEventListener('ontouchend', function() {
       mouseEntered = false;
-      window.requestAnimationFrame(step);
+      
+      setTimeout(function() {
+         window.requestAnimationFrame(step);
+      }, delay)
+
    })
    
    
    function updateImage(imageNo) {
       let image_url = '/assets/images/0'+imageNo+'.jpg';
-   
+
       image.setAttribute('src', image_url)
-   
+      
    }
    
    function step() {
@@ -38,17 +49,27 @@ window.addEventListener('DOMContentLoaded', (event) => {
          imageNo++;
        
          if (imageNo <= 84) {
-           window.requestAnimationFrame(step);
+            setTimeout(function() {
+               window.requestAnimationFrame(step);
+            }, delay)
+           
          } else {
             imageNo = 1;
-            window.requestAnimationFrame(step);
+            
+            setTimeout(function() {
+               window.requestAnimationFrame(step);
+            }, delay)
          }
    
       }
    
    }
+
+   setTimeout(function() {
+      window.requestAnimationFrame(step);
+   }, delay)
    
-   window.requestAnimationFrame(step);
+   
    
 });
 
