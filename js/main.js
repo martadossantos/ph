5020497827563830cbf7583e2b90e20b58b8de4b
piggypatrol <68651278+piggypatrol@users.noreply.github.com>
2022-04-28 18:20:34 +0100

    remove dom loaded

--- js/main.js
+++ js/main.js
@@ -3,77 +3,70 @@ let imageNo = 0;
 
 let delay = 250;
 
-mouseEntered = false
+mouseEntered = false;
 
-window.addEventListener('DOMContentLoaded', (event) => {
 
-   image.addEventListener('mouseenter', function() {
-      mouseEntered = true
-   })
-   
-   image.addEventListener('ontouchstart', function() {
-      mouseEntered = true
-   })
-   
-   image.addEventListener('mouseleave', function() {
-      mouseEntered = false;
-      
-      setTimeout(function() {
-         window.requestAnimationFrame(step);
-      }, delay)
+image.addEventListener('mouseenter', function() {
+   mouseEntered = true
+})
 
-   })
-   
-   image.addEventListener('ontouchend', function() {
-      mouseEntered = false;
-      
-      setTimeout(function() {
-         window.requestAnimationFrame(step);
-      }, delay)
+image.addEventListener('ontouchstart', function() {
+   mouseEntered = true
+})
 
-   })
+image.addEventListener('mouseleave', function() {
+   mouseEntered = false;
    
-   
-   function updateImage(imageNo) {
-      let image_url = '/assets/images/0'+imageNo+'.jpg';
-
-      image.setAttribute('src', image_url)
-      
-   }
-   
-   function step() {
-   
-      if (mouseEntered === false) {
-   
-         updateImage(imageNo);
-         imageNo++;
-       
-         if (imageNo <= 84) {
-            setTimeout(function() {
-               window.requestAnimationFrame(step);
-            }, delay)
-           
-         } else {
-            imageNo = 1;
-            
-            setTimeout(function() {
-               window.requestAnimationFrame(step);
-            }, delay)
-         }
-   
-      }
-   
-   }
-
    setTimeout(function() {
       window.requestAnimationFrame(step);
    }, delay)
+
+})
+
+image.addEventListener('ontouchend', function() {
+   mouseEntered = false;
    
-   
-   
-});
+   setTimeout(function() {
+      window.requestAnimationFrame(step);
+   }, delay)
+
+})
 
 
+function updateImage(imageNo) {
+   let image_url = '/assets/images/0'+imageNo+'.jpg';
+
+   image.setAttribute('src', image_url)
+   
+}
+
+function step() {
+
+   if (mouseEntered === false) {
+
+      updateImage(imageNo);
+      imageNo++;
+    
+      if (imageNo <= 84) {
+         setTimeout(function() {
+            window.requestAnimationFrame(step);
+         }, delay)
+        
+      } else {
+         imageNo = 1;
+         
+         setTimeout(function() {
+            window.requestAnimationFrame(step);
+         }, delay)
+      }
+
+   }
+
+}
+
+setTimeout(function() {
+   window.requestAnimationFrame(step);
+}, delay)
 
 
 
